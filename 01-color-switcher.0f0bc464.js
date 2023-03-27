bbcadea3cdd092185264213b3c771be4fb386a6e
olya-fedorchenko <114523005+olya-fedorchenko@users.noreply.github.com>
2023-03-27 10:38:51 +0000

    Deploying to gh-pages from @ olya-fedorchenko/goit-js-hw-09@33175b4af52dacdaf82db148966a9d7ea7471d7f 🚀

---
+++ 01-color-switcher.0f0bc464.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");let o=null;t.addEventListener("click",(()=>{t.setAttribute("disabled",""),o=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3)})),e.addEventListener("click",(()=>{clearInterval(o),t.removeAttribute("disabled")}));
+//# sourceMappingURL=01-color-switcher.0f0bc464.js.map
